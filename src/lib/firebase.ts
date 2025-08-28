f11f54afd0cf9dcc1c82d837372a9d6f901c51e3
admin ap <admin@philipabin.com>
2025-08-28 14:14:53 +0000

    FirebaseError: Firebase Storage: Max retry time for operation exceeded,

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
@@ -5,7 +6,7 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   "projectId": "roleplay-nvrtl",
   "appId": "1:922547464617:web:98be7ff915ea60b29ffcb8",
-  "storageBucket": "roleplay-nvrtl.appspot.com", // IMPORTANT: Replace with your actual bucket name if different
+  "storageBucket": "roleplay-nvrtl.appspot.com",
   "apiKey": "AIzaSyAKAzFfQnKSicH4jcuC3uSxErzAqJcrhn0",
   "authDomain": "roleplay-nvrtl.firebaseapp.com",
   "measurementId": "",
