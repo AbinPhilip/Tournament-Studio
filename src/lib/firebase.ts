8d1d6c601a6d27fcdc37fb1c74e661d79376c1f4
admin ap <admin@philipabin.com>
2025-09-06 06:08:09 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   "projectId": "roleplay-nvrtl",
   "appId": "1:922547464617:web:98be7ff915ea60b29ffcb8",
-  "storageBucket": "roleplay-nvrtl.appspot.com",
+  "storageBucket": "roleplay-nvrtl.firebasestorage.app",
   "apiKey": "AIzaSyAKAzFfQnKSicH4jcuC3uSxErzAqJcrhn0",
   "authDomain": "roleplay-nvrtl.firebaseapp.com",
   "measurementId": "",
